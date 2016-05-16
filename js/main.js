1edb1684274d70e6fd7fe9bdad4693204f1ccb8c
Adam Weeks <aoweeks@gmail.com>
2016-05-16 21:13:07 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,151 @@
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top - 99//101 rather than 100 because of issue where 1 pixel was still sometimes visible
+        },500);
+    }
+
+});
+
+$(window).scroll(function (event) { 
+    
+    updateBasedOnScrollPosition();
+    
+});
+
+
+$(document).ready(function (event){
+    updateBasedOnScrollPosition();
+});
+
+$('#murderthon-trailer').on('click', function(){
+    console.log("MURDERTHON!!!");
+   $('#youtube-screen').html(`<iframe xmlns="http://www.w3.org/1999/xhtml"
+                                        width="788.27" height="443.402"
+                                        src="https://www.youtube.com/embed/Zk-hz7ki4H8"
+                                        frameborder="0"></iframe>`);
+});
+
+
+sectionTops = [];
+function sectionTopCalculator(){
+    
+}
+
+
+
+var avatar = new Snap('.avatar-content');
+Snap.load('img/jack.svg', function (response) {
+   
+   var theMonitor = response.select('#monitor-group');
+   theMonitor.click( monitorClickHandler );
+   
+   avatar.append(response);
+});
+
+
+
+var monitorOn = true;
+function monitorClickHandler(){
+    console.log("Monitor clicked!");
+    monitorOn ? $('#monitor-screen-on').css("opacity", 1) : $('#monitor-screen-on').css("opacity", 0);
+    monitorOn = !monitorOn;
+};
+
+
+
+var posterStand = new Snap('#poster-stand');
+Snap.load('img/poster-stand.svg', function (response) {
+    posterStand.append(response);
+});
+
+var thePoster = posterStand.image("img/murderthon.jpg", 181.386, -118.162, 542.798, 767.714);
+
+
+
+
+
+var headIcon = new Snap('.jack-head-icon');
+Snap.load('img/jack-head.svg', function (response) {
+   headIcon.append(response); 
+});
+
+
+var calendarIcon = new Snap('.calendar-icon');
+Snap.load('img/calendar.svg', function (response) {
+   calendarIcon.append(response); 
+});
+
+
+var televisionIcon = new Snap('.television-icon');
+Snap.load('img/television.svg', function (response) {
+   televisionIcon.append(response); 
+});
+
+
+var envelopeIcon = new Snap('.envelope-icon');
+Snap.load('img/envelope.svg', function (response) {
+   envelopeIcon.append(response); 
+});
+
+function updateBasedOnScrollPosition(){
+    
+  
+    
+    var windowTop = $(document).scrollTop();
+    var halfWindowHeight = $(window).height() / 2;
+    
+    var aboutPoint = $("#about").position().top - halfWindowHeight;
+    var datesPoint = $("#dates").position().top - halfWindowHeight;
+    var mediaPoint = $("#media").position().top - halfWindowHeight;
+    var contactPoint = $("#contact").position().top - halfWindowHeight;
+    
+    
+    
+   console.log(windowTop);
+    
+    /* Could do with refactoring to DRY up*/
+    if(windowTop < aboutPoint){
+        var topColour = $('#top').css("background-color");
+        $(".title-cap").css("color", topColour);
+        $("#lab-background").css("opacity", 0);
+        
+    }
+    else if(windowTop > aboutPoint && windowTop < datesPoint){
+        var aboutColour = $('#about').css("background-color");
+        $(".title-cap").css("color", aboutColour);
+        $("#lab-background").css("opacity", 1);
+        $("#comedy-club-background").css("opacity", 0);
+        $("#comedy-club-foreground").css("opacity", 0);
+    }
+    else if(windowTop > datesPoint && windowTop < mediaPoint){
+        var datesColour = $('#dates').css("background-color");
+        $(".title-cap").css("color", datesColour);
+        $("#lab-background").css("opacity", 0);
+        $("#comedy-club-background").css("opacity", 1);
+        $("#comedy-club-foreground").css("opacity", 1);
+        $("#contact-background").css("opacity", 0);
+        
+    }
+    else if(windowTop > mediaPoint && windowTop < contactPoint){
+        var mediaColour = $('#media').css("background-color");
+        $(".title-cap").css("color", mediaColour);
+        $("#comedy-club-background").css("opacity", 0);
+        $("#comedy-club-foreground").css("opacity", 0);
+        $("#contact-background").css("opacity", 0);
+        
+    }
+    
+    else if(windowTop > contactPoint){
+        var contactColour = $('#contact').css("background-color");
+        $(".title-cap").css("color", contactColour);
+        $("#comedy-club-background").css("opacity", 0);
+        $("#comedy-club-foreground").css("opacity", 0);
+        $("#contact-background").css("opacity", 1);
+    }
+    
+}
