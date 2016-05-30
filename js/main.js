0adeb48b8fa844c7fcd3edbf226639f8953c3304
Adam Weeks <aoweeks@gmail.com>
2016-05-30 15:06:47 +0100

    Calendar icon shows current date

--- js/main.js
+++ js/main.js
@@ -41,7 +41,6 @@ var disableScrollAtStart = true;
 
 $(window).scroll( function (event) {
     var goingDown;
-    console.log("SCROLLING");
     
     goingDown = $(document).scrollTop() > lastScrollPos ? true : false;
     
@@ -66,6 +65,7 @@ $(document).ready( function (event){
     setTimeout(function(){
         disableScrollAtStart = false;
     }, 300);
+    
 });
 
 /* SECTION SCROLLING STUFF
@@ -176,7 +176,6 @@ function placeYouTubeScreen(){
     var screenLeft = $('#youtube-screen-off').position().left;
     var screenTop = $('#youtube-screen-off').position().top;
     
-    console.log(screenLeft + " " + screenTop + " : " + screenHeight + " " + screenWidth);
     
     $('#youtube-screen-on').css('left', screenLeft);
     $('#youtube-screen-on').css('top', screenTop);
@@ -291,7 +290,6 @@ function movePostersUp(){
     
     posterMask.animate({ transform: moveUpMatrix}, 3000 );
     posters.animate({ transform: moveDownMatrix}, 3000 );
-    console.log(posterHeight);
     setTimeout(movePosters, 3000);
     
 }
@@ -745,11 +743,99 @@ Snap.load('img/jack-head.svg', function (response) {
    headIcon.append(response); 
 });
 
-
+var topPage;
+var topMonth;
+var topDay;
+var bottomMonth;
+var bottomDay;
 var calendarIcon = new Snap('.calendar-icon');
 Snap.load('img/calendar.svg', function (response) {
-   calendarIcon.append(response); 
+    
+   var today = new Date(); 
+   
+   var todayMonthString = getMonthString(today.getMonth());
+   var todayDay = today.getDate();
+   
+   
+   var tomorrow = new Date();
+   tomorrow.setDate(todayDay + 1);
+   
+   var tomorrowMonthString = getMonthString(tomorrow.getMonth());
+   var tomorrowDay = tomorrow.getDate();
+   
+   topPage = response.select('#top-page');
+   topMonth = response.select('#top-month');
+   topDay = response.select('#top-day');
+
+   topMonth.attr({text: todayMonthString});
+   topDay.attr({text: todayDay});
+   
+   bottomMonth = response.select('#bottom-month');
+   bottomDay = response.select('#bottom-day');
+
+   bottomMonth.attr({text: tomorrowMonthString});
+   bottomDay.attr({text: tomorrowDay});
+   
+   calendarIcon.append(response);
+   
+   centerCalendarDates();
+   
+   /*Ugly workaround for centering month date which sometimes gives wrong value at first,
+   must FIX LATER*/
+   setTimeout(centerCalendarDates, 0);
 });
+
+function centerCalendarDates(){
+       
+   var topPageCenter = topPage.getBBox().width / 2;
+   var topMonthHalf = topMonth.getBBox().width / 2;
+   var topDayHalf = topDay.getBBox().width / 2;
+   
+   //console.log(topMonthHalf);
+   //console.log(topDayHalf);
+   
+   topMonth.attr({x: topPageCenter - topMonthHalf});
+   topDay.attr({x: topPageCenter - topDayHalf});
+   //console.log("CENTERED " + topMonthHalf);
+   
+   var bottomMonthHalf = bottomMonth.getBBox().width / 2;
+   var bottomDayHalf = bottomDay.getBBox().width / 2;
+   
+   
+   bottomMonth.attr({x: topPageCenter - bottomMonthHalf});
+   bottomDay.attr({x: topPageCenter - bottomDayHalf});
+}
+
+function getMonthString(monthNumeric){
+       
+   switch(monthNumeric) {
+       case 0:
+          return "JAN";
+       case 1:
+          return "FEB";
+       case 2:
+          return "MAR";
+       case 3:
+          return "APR";
+       case 4:
+          return "MAY";
+       case 5:
+          return "JUN";
+       case 6:
+          return "JUL";
+       case 7:
+          return "AUG";
+       case 8:
+          return "SEP";
+       case 9:
+          return "OCT";
+        case 10:
+          return "NOV";
+       case 11:
+          return "DEC";
+      
+   }
+}
 
 
 var televisionIcon = new Snap('.television-icon');
